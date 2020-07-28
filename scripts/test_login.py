229162b6f9c341581ba3b2dee3b01f6b22f14f1e
lyz9677 <1197500916@qq.com>
2020-07-28 19:23:53 +0800

    新建项目

---
+++ scripts/test_login.py
@@ -0,0 +1,7 @@
+import pytest
+class TestLogin:
+    def test_a(self):
+        print('这是 test_a')
+
+    def test_b(self):
+        print('这是 test_b')
---
+++ scripts/test_out.py
@@ -0,0 +1,7 @@
+class TestOut:
+    def test_c(self):
+        print('这是 test_out_a')
+
+    def test_d(self):
+        print('这是 test_out_b')
+        assert False
